c5f11c4862c13e4cce9675e2749f44de50559fb5
vladimirovmm <vladimirov.m.m@ya.ru>
2021-07-02 18:52:22 +0500

    fix for mac

--- dove_download.sh
+++ dove_download.sh
@@ -8,7 +8,8 @@ if [ ! -e $basefolder ]; then
 fi
 releases_path="$basefolder/releases.json"
 
-if [ ! -e $releases_path ] || [ $(expr $(stat -c %Y "$releases_path") + 600) -le $(date +%s) ]; then
+differencetime=$(($(date "+%s")-$(date -r $releases_path "+%s" )))
+if [ ! -e $releases_path ] || [ $differencetime -ge 600 ]; then
   echo "Download: releases.json"
   curl -o "$releases_path" \
       -s https://api.github.com/repos/pontem-network/move-tools/releases
@@ -18,8 +19,7 @@ dove_version=""
 if [[ $1 == "latest" || $1 == "new" || $1 == "last" || -z $1 ]]; then
   dove_version=$(cat "$releases_path" | jq -r '.[0] .tag_name')
 else
-  if [ ! $(cat "$releases_path" |
-    jq ".[] | select(.tag_name==\"${1}\") .tag_name") ]; then
+  if [ ! $(cat "$releases_path" | jq ".[] | select(.tag_name==\"${1}\") .tag_name") ]; then
     echo "{$1} The specified version of dove was not found"
     exit 1
   fi
@@ -43,8 +43,14 @@ file_path="$basefolder/$filename"
 download_url=$(cat "$releases_path" |
   jq -r ".[] | select(.tag_name==\"${dove_version}\") .assets | .[] | select(.name|test(\"^dove-${dove_version}-${download_type}\")) | .browser_download_url")
 if [ -z $download_url ]; then
-  echo "Releases \"dove-${dove_version}-${download_type}\" not found"
-  exit 3
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+      download_url=$(cat "$releases_path" |
+        jq -r ".[] | select(.tag_name==\"${dove_version}\") .assets | .[] | select(.name|test(\"^dove-${dove_version}-mac-${HOSTTYPE}\")) | .browser_download_url")
+  fi
+  if [ -z $download_url ]; then
+    echo "Releases \"dove-${dove_version}-${download_type}\" not found"
+    exit 3
+  fi
 fi
 
 if [ ! -e $file_path ]; then
