6f0369a4811f5918fa73fb0e607e10f15ae9e72f
Максим Владимиров <32911557+vladimirovmm@users.noreply.github.com>
2021-07-05 14:24:01 +0500

    fix for mac + add GITHUB_TOKEN (#3)
    * fix for mac
    
    * add optional GITHUB_TOKEN
    
    * update README

--- dove_download.sh
+++ dove_download.sh
@@ -8,18 +8,24 @@ if [ ! -e $basefolder ]; then
 fi
 releases_path="$basefolder/releases.json"
 
-if [ ! -e $releases_path ] || [ $(expr $(stat -c %Y "$releases_path") + 600) -le $(date +%s) ]; then
+if [ ! -e $releases_path ] || [ $(($(date "+%s")-$(date -r $releases_path "+%s" ))) -ge 600 ]; then
   echo "Download: releases.json"
-  curl -o "$releases_path" \
-      -s https://api.github.com/repos/pontem-network/move-tools/releases
+  if [ -z $2 ]; then
+    curl -o "$releases_path.tmp" \
+        -s https://api.github.com/repos/pontem-network/move-tools/releases
+  else
+    curl -o "$releases_path.tmp" \
+        -H "Authorization: Bearer ${2}" \
+        -s https://api.github.com/repos/pontem-network/move-tools/releases
+  fi
+  mv "$releases_path.tmp" $releases_path
 fi
 
 dove_version=""
 if [[ $1 == "latest" || $1 == "new" || $1 == "last" || -z $1 ]]; then
   dove_version=$(cat "$releases_path" | jq -r '.[0] .tag_name')
 else
-  if [ ! $(cat "$releases_path" |
-    jq ".[] | select(.tag_name==\"${1}\") .tag_name") ]; then
+  if [ ! $(cat "$releases_path" | jq ".[] | select(.tag_name==\"${1}\") .tag_name") ]; then
     echo "{$1} The specified version of dove was not found"
     exit 1
   fi
@@ -43,16 +49,31 @@ file_path="$basefolder/$filename"
 download_url=$(cat "$releases_path" |
   jq -r ".[] | select(.tag_name==\"${dove_version}\") .assets | .[] | select(.name|test(\"^dove-${dove_version}-${download_type}\")) | .browser_download_url")
 if [ -z $download_url ]; then
-  echo "Releases \"dove-${dove_version}-${download_type}\" not found"
-  exit 3
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+      download_url=$(cat "$releases_path" |
+        jq -r ".[] | select(.tag_name==\"${dove_version}\") .assets | .[] | select(.name|test(\"^dove-${dove_version}-mac-${HOSTTYPE}\")) | .browser_download_url")
+  fi
+  if [ -z $download_url ]; then
+    echo "Releases \"dove-${dove_version}-${download_type}\" not found"
+    exit 3
+  fi
 fi
 
 if [ ! -e $file_path ]; then
   echo "Download: $download_url"
-  curl -sL --fail \
-    -H "Accept: application/octet-stream" \
-    -o $file_path \
-    -s $download_url
+  if [ -z $2 ]; then
+    curl -sL --fail \
+      -H "Accept: application/octet-stream" \
+      -o "$file_path.tmp" \
+      -s $download_url
+  else
+    curl -sL --fail \
+      -H "Accept: application/octet-stream" \
+      -H "Authorization: Bearer ${2}" \
+      -o "$file_path.tmp" \
+      -s $download_url
+  fi
+  mv "$file_path.tmp" $file_path
 fi
 
 echo "chmod 1755 $file_path"
